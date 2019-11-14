b07d7ad06971d85559a7c89a7e587b4bebe816a9
Sumit Yadao <sumit.yadao@volkswagen.co.in>
2019-11-14 23:12:09 +0530

    Add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AddEmployeeComponent} from './add-employee/add-employee.component';
+import {ViewEmployeeComponent} from './view-employee/view-employee.component';
+import {UpdateEmployeeComponent} from './update-employee/update-employee.component';
+import {DeleteEmployeeComponent} from './delete-employee/delete-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add', component: AddEmployeeComponent},
+  {path: 'view', component: ViewEmployeeComponent},
+  {path: 'update', component: UpdateEmployeeComponent},
+  {path: 'delete', component: DeleteEmployeeComponent},
+  {path: '', component: AddEmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    ViewEmployeeComponent,
+    UpdateEmployeeComponent,
+    DeleteEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
